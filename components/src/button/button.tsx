3b3d6802935b209e5504b7da62d45ec930023ca8
Jeremiah Brannon <jdbrannon@wustl.edu>
2023-10-05 17:11:46 -0500

    spread button props

--- components/src/button/button.tsx
+++ components/src/button/button.tsx
@@ -13,16 +13,17 @@ export type ButtonProps = HtmlHTMLAttributes<HTMLButtonElement> & {
 }
 
 export function Button(props: ButtonProps) {
-  const { children, variant = 'contained', size = 'medium', color = 'primary', pill = false } = props;
+  const { children, variant = 'contained', size = 'medium', color = 'primary', pill = false, ...rest } = props;
 
   return (
-    <button 
+    <button
       className={`
         ${styles.base}
         ${styles[`${color}-${variant}`]}
         ${styles[variant]}
         ${styles[size]}
-        ${pill ? styles.pill : ""}`}>
+        ${pill ? styles.pill : ""}`}
+      {...rest}>
       {children}
     </button>
   ) 
