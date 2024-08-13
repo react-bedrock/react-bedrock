be0d1a9e187f63d4d0600004d3d7429382933279
Jeremiah Brannon <jdbrannon@wustl.edu>
2024-08-13 16:09:01 -0600

    #2: Add default shadows and paper elevation

--- libs/components/src/paper/paper.tsx
+++ libs/components/src/paper/paper.tsx
@@ -2,17 +2,20 @@ import { HtmlHTMLAttributes, PropsWithChildren } from "react";
 import styles from './paper.module.css';
 
 export type PaperCorners = "rounded" | "square";
+export type PaperElevation = "0" | "1" | "2" | "3" | "4" | "6" | "8" | "12" | "16" | "24";
 
 export type PaperProps = HtmlHTMLAttributes<HTMLDivElement> & PropsWithChildren & {
   corners?: PaperCorners;
+  elevation?: PaperElevation;
 }
 
 export function Paper(props: PaperProps) {
-  const { corners = "rounded", children, ...rest } = props;
+  const { corners = "rounded", elevation = "4", children, ...rest } = props;
 
   function getStyles(): string {
     const styleStrings = [
-      styles[corners]
+      styles[corners],
+      styles[`elevation-${elevation}`]
     ];
 
     return styleStrings.join(' ');
