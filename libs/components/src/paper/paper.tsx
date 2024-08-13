88caed069e0835c010e9c714575179027a048153
Jeremiah Brannon <jdbrannon@wustl.edu>
2024-08-13 17:07:26 -0600

    #2: Change elevation to numbers

--- libs/components/src/paper/paper.tsx
+++ libs/components/src/paper/paper.tsx
@@ -2,7 +2,7 @@ import { HtmlHTMLAttributes, PropsWithChildren } from "react";
 import styles from './paper.module.css';
 
 export type PaperCorners = "rounded" | "square";
-export type PaperElevation = "0" | "1" | "2" | "3" | "4" | "6" | "8" | "12" | "16" | "24";
+export type PaperElevation = 0 | 1 | 2 | 3 | 4 | 6 | 8 | 12 | 16 | 24;
 export type PaperVariant = "default" | "outlined";
 
 export type PaperProps = HtmlHTMLAttributes<HTMLDivElement> & PropsWithChildren & {
@@ -12,7 +12,7 @@ export type PaperProps = HtmlHTMLAttributes<HTMLDivElement> & PropsWithChildren 
 }
 
 export function Paper(props: PaperProps) {
-  const { corners = "rounded", elevation = "4", variant = "default", children, ...rest } = props;
+  const { corners = "rounded", elevation = 4, variant = "default", children, ...rest } = props;
 
   function getStyles(): string {
     const styleStrings = [
