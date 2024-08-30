31793d4684182fe70980a1a913c8d76de1c8e634
Jeremiah Brannon <jdbrannon@wustl.edu>
2024-08-29 18:02:31 -0600

    #14: Set default html elements

--- apps/styleguide/app/nav-bar/nav-bar.tsx
+++ apps/styleguide/app/nav-bar/nav-bar.tsx
@@ -5,7 +5,7 @@ import { NavBarItem } from './nav-bar-item'
 export function NavBar() {
   return (
     <div className={styles['nav-bar']}>
-      <h1 className={styles['nav-bar-title']}>React Bedrock</h1>
+      <h3 className={styles['nav-bar-title']}>React Bedrock</h3>
       <NavBarItems>
         <NavBarItem href="/">Dashboard</NavBarItem>
         <NavBarItem href="/components/button">Button</NavBarItem>
